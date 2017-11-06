c80f21a32d45028671555fd5c133527a92b8509b
Luke Bridges <LukeBridges@users.noreply.github.com>
2017-11-06 13:55:04 +0000

    Add files via upload

---
+++ Date.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OPG.Dates
+{
+    public sealed class OPGDate
+    {
+        private static DateTime now = DateTime.UtcNow;
+
+        public static string GetHTTPHeader()
+        {
+            return "Date: " + now.DayOfWeek.ToString().Substring(0,3) + ", " + now.Day + " " + now.Month + " "  + now.Year + " " + now.Hour + ":" + now.Minute + ":" + now.Second + " GMT\r\n";
+        }
+
+        public OPGDate()
+        {
+
+        }
+
+        public OPGDate(int dateIn)
+        {
+            now = new DateTime(dateIn);
+        }
+    }
+}
